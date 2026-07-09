f97d6c4e4942ba8e91bc083fd9e65c6c4ac3708b
Rishika Vunnama <rvunnama@gmail.com>
2026-07-09 12:48:26 -0500

    Add registraton form

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        print("Username:", username)
+        print("Password:", password)
+
+        return "Registration form submitted!"
+
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
